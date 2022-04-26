8c40b2a8fc553ba69e43c274b29f9ca0c0bcaa01
Mahdi Behnam <mahdiscorpio3851099@gmail.com>
2022-04-26 21:37:36 +0430

    main  tutorial part 3 completed (consumers have been converted to be asyncronous)

--- base/consumers.py
+++ base/consumers.py
@@ -1,33 +1,31 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
-from asgiref.sync import async_to_sync
-import os
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = "chat_%s" % self.room_name
         # Join Room Group:
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name)
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave Room Group:
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Recieve Message from Websocket:
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -36,10 +34,10 @@ class ChatConsumer(WebsocketConsumer):
         )
 
     # Receive Message from Room Group:
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
 
         # Send Message to Websocket:
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message,
         }))
