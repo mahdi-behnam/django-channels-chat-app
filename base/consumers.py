a6ff79a65a690c449a2a03a0b475b15724602e54
Mahdi Behnam <mahdiscorpio3851099@gmail.com>
2022-04-26 21:29:41 +0430

    main  tutorial part 2 completed(async chat is functional and completed)

--- base/consumers.py
+++ base/consumers.py
@@ -1,18 +1,45 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+import os
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = "chat_%s" % self.room_name
+        # Join Room Group:
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name)
+
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # Leave Room Group:
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
+    # Recieve Message from Websocket:
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    # Receive Message from Room Group:
+    def chat_message(self, event):
+        message = event['message']
+
+        # Send Message to Websocket:
         self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
         }))
--- chat_app/settings.py
+++ chat_app/settings.py
@@ -74,7 +74,16 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'chat_app.wsgi.application'
+# Channels
 ASGI_APPLICATION = 'chat_app.asgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
