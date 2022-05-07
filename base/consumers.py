c1247f0075688884fa39e389012339f938467079
Mahdi Behnam <mahdiscorpio3851099@gmail.com>
2022-05-07 20:35:45 +0430

    main  video call done

--- base/consumers.py
+++ base/consumers.py
@@ -25,21 +25,22 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # Recieve Message from Websocket:
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message_text = text_data_json['message']
-        message_user = text_data_json['user']
-
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message_text': message_text,
-                'message_user': message_user,
-            }
-        )
-        await self.create_message(text=message_text)
+        print(text_data_json.get('message'))
+        # If the websocket has sent a message:
+        if(text_data_json.get('message')):
+            message_text = text_data_json.get('message')
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'message_received',
+                    'message_text': message_text,
+                    'message_user': self.scope['user'].username,
+                }
+            )
+            await self.create_message(text=message_text)
 
     # Receive Message from Room Group:
-    async def chat_message(self, event):
+    async def message_received(self, event):
         # Send Message to Websocket:
         await self.send(text_data=json.dumps({
             'message': event.get('message_text'),
@@ -47,8 +48,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     # Receive File from Room Group:
     async def file_received(self, event):
-        print("FILEURL RECEIVEDDDDDDD")
-        print(event)
         # Send FileURL to Websocket:
         await self.send(text_data=json.dumps({
             'user': event.get('user'),
@@ -62,3 +61,119 @@ class ChatConsumer(AsyncWebsocketConsumer):
     def create_message(self, text):
         room = Room.objects.get(id=self.room_id)
         return Message.objects.create(user=self.scope['user'], room=room, text=text)
+
+
+class CallConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_id = self.scope['url_route']['kwargs']['room_id']
+        self.room_group_name = "call_%s" % (self.room_id)
+        # Join Room Group:
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name)
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave Room Group:
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Recieve Message from Websocket:
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        event_type = text_data_json.get('type')
+        data = text_data_json.get('data')
+        print("````````````````````````````````````")
+        print(data)
+
+        # If the websocket has sent ice candidates:
+        if(event_type == 'ice'):
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'ice_received',
+                    'ice': data.get('ice'),
+                    'sender': self.scope['user'].username,
+                })
+
+        # If the websocket has sent an offer to start a call:
+        elif(event_type == 'offer'):
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'offer_received',
+                    'offer': data.get('offer'),
+                    'sender': self.scope['user'].username,
+                })
+
+        # If the websocket has sent an answer to start a call:
+        elif(event_type == 'answer'):
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'answer_received',
+                    'answer': data.get('answer'),
+                    'sender': self.scope['user'].username,
+                })
+        elif(event_type == "call_status"):
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'call_status_received',
+                    'status': data.get('status'),
+                    'sender': self.scope['user'].username,
+                }
+            )
+
+    # Receive Call Offer from Room Group:
+    async def offer_received(self, event):
+        # If the offer's sender is not the same as this socket's target, send the offer...
+        if(self.scope['user'].username != event.get('sender')):
+            await self.send(
+                text_data=json.dumps(
+                    {
+                        'offer': event.get('offer'),
+                        'sender': event.get('sender'),
+                    }
+                )
+            )
+
+    # Receive Call Answer from Room Group:
+    async def answer_received(self, event):
+        # If the answer's sender is not the same as this socket's target, send the answer...
+        if(self.scope['user'].username != event.get('sender')):
+            await self.send(
+                text_data=json.dumps(
+                    {
+                        'answer': event.get('answer'),
+                        'sender': event.get('sender'),
+                    }
+                )
+            )
+
+    # Receive Ice Candidates from Room Group:
+    async def ice_received(self, event):
+        # If the ice's sender is not the same as this socket's target, send the ice...
+        if(self.scope['user'].username != event.get('sender')):
+            await self.send(
+                text_data=json.dumps(
+                    {
+                        'ice': event.get('ice'),
+                        'sender': event.get('sender'),
+                    }
+                )
+            )
+
+    # Receive Call Status from Room Group:
+    async def call_status_received(self, event):
+        if(self.scope['user'].username != event.get('sender')):
+            await self.send(
+                text_data=json.dumps(
+                    {
+                        'status': event.get("status"),
+                    }
+                )
+            )
---
+++ base/static/scripts/main.js
@@ -0,0 +1,233 @@
+//Messages Socket Config:
+const roomID = JSON.parse(document.getElementById("room-id").textContent);
+const chatSocket = new WebSocket(
+  "ws://" + window.location.host + `/ws/chat/${roomID}/`
+);
+chatSocket.onmessage = function (e) {
+  const data = JSON.parse(e.data);
+  console.log("DATA RECEIVED AND IS: ", data);
+
+  if (data.hasOwnProperty("message")) {
+    strToAdd = `${data.user}: ${data.message}<br />`;
+    document.querySelector("#chat-log").innerHTML += strToAdd;
+  }
+  //Else If a File has been received...
+  else if (data.hasOwnProperty("file_type")) {
+    if (data.file_type === "image")
+      strToAdd = `${data.user}: <img src="${data.file_url}" width="200" height="200" /><br />`;
+    else if (data.file_type === "video")
+      strToAdd = `${data.user}: <video width="200" height="200" controls><source src="${data.file_url}"/></video><br />`;
+    else
+      strToAdd = `${data.user}: <a href="${data.file_url}">${data.file_name}</a><br />`;
+    document.querySelector("#chat-log").innerHTML += strToAdd;
+  }
+};
+chatSocket.onclose = function (e) {
+  console.error("Chat socket closed unexpectedly");
+};
+document.querySelector("#chat-message-input").focus();
+document.querySelector("#chat-message-input").onkeyup = function (e) {
+  if (e.keyCode === 13) {
+    // enter, return
+    document.querySelector("#chat-message-submit").click();
+  }
+};
+document.querySelector("#chat-message-submit").onclick = function (e) {
+  const messageInputDom = document.querySelector("#chat-message-input");
+  const message = messageInputDom.value;
+  chatSocket.send(
+    JSON.stringify({
+      message: message,
+    })
+  );
+  messageInputDom.value = "";
+};
+
+//MEDIA FORM
+document.querySelector("#media-form").addEventListener("submit", (e) => {
+  const form = e.target;
+  fetch(form.action, {
+    method: form.method,
+    body: new FormData(form),
+  }).catch((e) => console.log(e));
+  e.preventDefault();
+});
+
+//Video Chat Config:
+const callSocket = new WebSocket(
+  "ws://" + window.location.host + `/ws/call/${roomID}/`
+);
+let peerConnection;
+let localStream;
+let remoteStream;
+let servers = {
+  iceServers: [
+    {
+      urls: [
+        "stun:stun1.l.google.com:19302",
+        "stun:stun2.l.google.com:19302",
+        "stun:stun3.l.google.com:19302",
+        "stun:stun4.l.google.com:19302",
+      ],
+    },
+  ],
+};
+
+const startCallBtn = document.getElementById("start-call");
+const endCallBtn = document.getElementById("end-call");
+
+callSocket.onmessage = function (e) {
+  const data = JSON.parse(e.data);
+  console.log("onmessage event received : => ", data);
+  //If an Offer to make a call has been received...
+  if (data.hasOwnProperty("offer")) {
+    console.log("offer received !");
+    makeAnswer(data.offer);
+  }
+  // Else If an Answer to a call has been received...
+  else if (data.hasOwnProperty("answer")) {
+    console.log("answer received !");
+    addAnswer(data.answer);
+  }
+  // Else If an Ice Candidate for a call has been received...
+  else if (data.hasOwnProperty("ice")) {
+    console.log("ice received !");
+    if (peerConnection) {
+      addIce(data.ice);
+    }
+  }
+  // Else If a call status event has been received...
+  else if (data.hasOwnProperty("status")) {
+    console.log("call status has been received !");
+    if (data.status === "ended") {
+      if (peerConnection) peerConnection.close();
+      stopTracks();
+    }
+  }
+};
+
+callSocket.onclose = function (e) {
+  console.error("Chat socket closed unexpectedly");
+};
+
+const setLocalStream = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+  document.getElementById("local-user").srcObject = localStream;
+};
+
+const createPeerConnection = async () => {
+  peerConnection = new RTCPeerConnection(servers);
+
+  remoteStream = new MediaStream();
+  document.getElementById("remote-user").srcObject = remoteStream;
+
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
+
+  peerConnection.ontrack = (event) => {
+    event.streams[0]
+      .getTracks()
+      .forEach((track) => remoteStream.addTrack(track));
+  };
+
+  peerConnection.onicecandidate = async (event) => {
+    if (event.candidate) {
+      callSocket.send(
+        JSON.stringify({
+          type: "ice",
+          data: {
+            ice: event.candidate,
+          },
+        })
+      );
+    }
+  };
+};
+
+const makeOffer = async () => {
+  await createPeerConnection();
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  callSocket.send(
+    JSON.stringify({
+      type: "offer",
+      data: {
+        offer: offer,
+      },
+    })
+  );
+};
+
+const makeAnswer = async (offer) => {
+  if (confirm("Incoming Call...")) {
+    await setLocalStream();
+    await createPeerConnection();
+    await peerConnection.setRemoteDescription(offer);
+
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+    callSocket.send(
+      JSON.stringify({
+        type: "answer",
+        data: {
+          answer: answer,
+        },
+      })
+    );
+  } else {
+    callSocket.send(
+      JSON.stringify({
+        type: "call_status",
+        data: {
+          status: "ended",
+        },
+      })
+    );
+  }
+};
+
+const addAnswer = async (answer) => {
+  await peerConnection.setRemoteDescription(answer);
+};
+
+const stopTracks = async () => {
+  localStream.getTracks().forEach((track) => track.stop());
+  document.getElementById("local-user").srcObject = null;
+  document.getElementById("remote-user").srcObject = null;
+};
+
+const endCall = async () => {
+  if (peerConnection.connectionState !== "closed") {
+    await peerConnection.close();
+    stopTracks();
+    callSocket.send(
+      JSON.stringify({
+        type: "call_status",
+        data: {
+          status: "ended",
+        },
+      })
+    );
+  }
+};
+
+const addIce = async (ice) => {
+  await peerConnection.addIceCandidate(ice);
+};
+
+document.getElementById("logger").onclick = () => {
+  console.log(peerConnection);
+  console.log(localStream);
+  console.log(remoteStream);
+};
+
+startCallBtn.onclick = async () => {
+  await setLocalStream();
+  await makeOffer();
+};
+
+endCallBtn.onclick = endCall;
